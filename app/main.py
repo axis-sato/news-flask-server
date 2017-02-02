caeb6a93915e5e26419e962262f4927ebb824543
Masahiko Sato <c8112002@gmail.com>
2017-02-03 00:20:53 +0900

    NginxとFlaskをuwsgi protocolでつなげる

--- app/main.py
+++ app/main.py
@@ -1,9 +1,7 @@
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello world"
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+@app.route('/')
+def index():
+    return "<span style='color:red'>I am app 1</span>"
