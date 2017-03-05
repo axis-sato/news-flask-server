a6ac4ecf1fabbf8e40a05cd6539a8d87f2bcfd3d
Masahiko Sato <c8112002@gmail.com>
2017-03-05 23:46:38 +0900

    Add logging configurations

--- app/main.py
+++ app/main.py
@@ -1,15 +1,19 @@
 from flask import Flask, jsonify
 from model.article import Article
-from model.database import db
+from model.database import setup_db
+from logger import setup_logger
 
 app = Flask(__name__)
-db.init_app(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://news:secret@db/news'
+setup_db(app)
+setup_logger(app)
 
 
 @app.route('/')
 def index():
     articles = Article.query.all()
+    app.logger.debug(articles)
+    app.logger.info(articles)
+    app.logger.warn(articles)
     app.logger.error(articles)
 
     return jsonify(articles=[a.serialize() for a in articles])
--- app/model/database.py
+++ app/model/database.py
@@ -1,3 +1,9 @@
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+
 db = SQLAlchemy()
+
+def setup_db(app: Flask):
+    db.init_app(app)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://news:secret@db/news'
