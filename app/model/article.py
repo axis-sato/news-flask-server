c865f205f5b0fffd1e54f3c2f67fd5282dfbfa96
Masahiko Sato <c8112002@gmail.com>
2017-05-10 23:38:10 +0900

    Tmp commit

--- app/model/article.py
+++ app/model/article.py
@@ -7,6 +7,7 @@ class Article(db.Model):
     body = db.Column(db.String(255))
     url = db.Column(db.String(255))
     thumbnail = db.Column(db.String(255))
+    tags = db.relationship('Tag', secondary='article_tags')
 
     def __init__(self, title, body, url, thumbnail):
         self.title = title
@@ -20,10 +21,44 @@ class Article(db.Model):
             'title': self.title,
             'body': self.body,
             'url': self.url,
-            'thumbnail': self.thumbnail
+            'thumbnail': self.thumbnail,
+            # 'tags': self.tags
         }
 
     def __repr__(self):
         return '<Article %r>' % self.title
 
+class ArticleTag(db.Model):
+    __tablename__ = 'article_tags'
+    article_id = db.Column(db.Integer, db.ForeignKey('articles.id'))
+    tag_id = db.Column(db.Integer, db.ForeignKey('tags.id'))
 
+    # def __init__(self):
+    #     pass
+
+    def serialize(self):
+        return {
+            'article_id': self.article_id,
+            'tag_id': self.tag_id,
+        }
+
+    def __repr__(self):
+        return f"ArticleTag {self.article_id}_{self.tag_id}"
+
+class Tag(db.Model):
+    __tablename__ = 'tags'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+    # article = db.relationship('Article', secondaly='article_tags')
+
+    def __init__(self, name):
+        self.name = name
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+        }
+
+    def __repr__(self):
+        return f"<Tag {self.name}>"
--- app/model/tag.py
+++ app/model/tag.py
@@ -4,6 +4,7 @@ class Tag(db.Model):
     __tablename__ = 'tags'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20))
+    # article = db.relationship('Article', secondaly='article_tags')
 
     def __init__(self, name):
         self.name = name
