9900c108db8b386a6c6c6302500786f8dcc3dd30
Masahiko Sato <c8112002@gmail.com>
2017-06-10 14:15:41 +0900

    Move site-packages into application home directory

--- docker/flask/docker-entrypoint.sh
+++ docker/flask/docker-entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 cd /app
-pip install -r ./requirements.txt
+
+# カレントディレクトリのsite-packagesを検索パスに追加
+site_packages_dir="/app/site-packages"
+echo $site_packages_dir > /usr/local/lib/python3.6/site-packages/usrlocal.pth
+
+pip install -r ./requirements.txt -t $site_packages_dir
 uwsgi --ini ./uwsgi.ini
