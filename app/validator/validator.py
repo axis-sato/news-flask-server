f982c022a87c9f727f192231e7ca920917f6ac9d
Masahiko Sato <c8112002@gmail.com>
2017-06-14 12:52:25 +0900

    Fix validation error message

--- app/validator/validator.py
+++ app/validator/validator.py
@@ -1,16 +1,26 @@
 from functools import wraps
-from jsonschema import Draft4Validator
-from flask import jsonify, request
+from jsonschema import Draft4Validator, ValidationError
+from flask import jsonify, request, current_app
 
+
+def _validate(schema, data):
+    validator = Draft4Validator(schema)
+    errors = [dict(
+        message=e.message,
+        params=[p for p in e.path],
+        reason=e.validator,
+        #schema=e.schema,
+        #validator_value=e.validator_value
+    ) for e in validator.iter_errors(data)]
+    return errors
 
 def validate_schema(schema):
-    validator = Draft4Validator(schema)
-
     def wrapper(fn):
         @wraps(fn)
         def wrapped(*args, **kwargs):
             input = request.get_json(force=True)
-            errors = [error.message for error in validator.iter_errors(input)]
+            errors = _validate(schema, input)
+            current_app.logger.debug(errors)
             if errors:
                 response = jsonify(dict(success=False,
                                         message="invalid input",
